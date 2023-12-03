bc1aca9317fbc2edfa54fb7d0767e42cd5a36040
Salohyy212 <zerambaysalohy@gmail.com>
2023-12-03 14:03:01 +0100

    feat: add test data

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,17 @@
 package main;
 
 import main.entity.Account;
+import main.entity.Currency;
+import main.entity.Transaction;
 import main.repository.AccountCrudOrepations;
+import main.repository.CurrencyCrudOrepations;
+import main.repository.TransactionCrudOperations;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -49,6 +54,31 @@ public class Main {
     public static void main(String[] args) {
         Main mainInstance = Main.getInstance();
         Connection conn = mainInstance.getConnection();
+
+        AccountCrudOrepations accountCrudOperations = new AccountCrudOrepations();
+        List<Account> allAccounts = accountCrudOperations.findAll();
+        System.out.println("All Accounts:");
+        allAccounts.forEach(System.out::println);
+        Account newAccount = new Account(4, "Elise account",100000.00, 1);
+        Account savedAccount = accountCrudOperations.save(newAccount);
+        System.out.println("New subscriber added: " + savedAccount);
+
+        CurrencyCrudOrepations currencyCrudOperations = new CurrencyCrudOrepations();
+        List<Currency> allCurrencies = currencyCrudOperations.findAll();
+        System.out.println("All Currencies:");
+        allCurrencies.forEach(System.out::println);
+        Currency newCurrency= new Currency(3, "Euro","€");
+        Currency savedCurrency = currencyCrudOperations.save(newCurrency);
+        System.out.println("New subscriber added: " + savedCurrency);
+
+
+        TransactionCrudOperations transactionCrudOperations = new TransactionCrudOperations();
+        List<Transaction> allTransactions = transactionCrudOperations.findAll();
+        System.out.println("All Currencies:");
+        allCurrencies.forEach(System.out::println);
+        Transaction newTransaction= new Transaction(3, 1000.00,LocalDate.now(),2);
+        Transaction savedTransaction = transactionCrudOperations.save(newTransaction);
+        System.out.println("New subscriber added: " + savedTransaction);
 
 
 
